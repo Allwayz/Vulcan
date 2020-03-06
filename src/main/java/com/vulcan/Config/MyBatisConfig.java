2701fb717fdb3b5a458d94fb599caed99c0ff0c4
Allwayz <47277350+Allwayz@users.noreply.github.com>
2020-03-07 07:54:40 +0800

    Add log Framework (#11)

---
+++ src/main/java/com/vulcan/Config/MyBatisConfig.java
@@ -0,0 +1,10 @@
+package com.vulcan.Config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.vulcan.model.mapper")
+public class MyBatisConfig {
+
+}
--- src/main/java/com/vulcan/VulcanApplication.java
+++ src/main/java/com/vulcan/VulcanApplication.java
@@ -1,14 +1,11 @@
 package com.vulcan;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@MapperScan("com.vulcan.*.mapper")
 @SpringBootApplication
 public class VulcanApplication {
     public static void main(String[] args) {
-        SpringApplication.run(VulcanApplication.class,args);
+        SpringApplication.run(VulcanApplication.class, args);
     }
 }
