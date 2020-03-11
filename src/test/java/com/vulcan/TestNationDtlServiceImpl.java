86a8ca18b4d4edecb89d6aac083e726fb4d81d48
Allwayz <47277350+Allwayz@users.noreply.github.com>
2020-03-11 17:55:41 +0800

    Allwayz (#13)
    * Add log Framework
    
    * Stable development version
    
    * Update API
    
    * Update API
    
    * Update API

--- src/test/java/com/vulcan/TestNationDtlServiceImpl.java
+++ src/test/java/com/vulcan/TestNationDtlServiceImpl.java
@@ -9,12 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class TestNationDtlServiceImpl {
     private Logger logger = LoggerFactory.getLogger(TestNationDtlServiceImpl.class);
